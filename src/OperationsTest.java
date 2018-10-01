a03a45a67da1a0dec0ad3b243ece5e5c3cea61f9
jbarron21 <43002703+jbarron21@users.noreply.github.com>
2018-10-01 08:34:27 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,24 @@
+
+public class OperationsTest {
+
+	public static void main(String[] args) 
+	{
+		int number; //Data type then variable name to declare a variable
+		number = 20;
+		number = number + 5;  // number = number + 5
+		System.out.println("when you add 5 it is \n" +number);
+		number = number /2; 
+		System.out.println("when you divide by 2 it is \n" + number);
+		number = number *4;
+		System.out.println("when you multiply by 4 it is \n" + number);
+		number = number - 6;
+		System.out.println("when you subtract 6 it is \n" + number);
+		number %= 2;
+		System.out.println("when you modular divide by 2 it is \n" + number);
+		
+		
+		
+	}
+
+}
+
